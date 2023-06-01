f535272b6fd59a35801cb6761fea00f1bfa1328b
Abhishek Malvadkar <abhishek@dev.com>
2023-06-01 11:08:06 +0530

    Problem-1 : Refactoring

--- java-8-shoot/src/main/java/com/java8/problem/Problem_1.java
+++ java-8-shoot/src/main/java/com/java8/problem/Problem_1.java
@@ -3,7 +3,6 @@ package com.java8.problem;
 import static java.util.stream.Collectors.toList;
 
 import java.util.List;
-import java.util.function.Predicate;
 
 public class Problem_1 {
 
@@ -14,15 +13,18 @@ public class Problem_1 {
 	
 	public static void main(String[] args) {
 		List<Integer> numbers = List.of(1 , 5 , 2 , 3 , 8 , 7);
-		Predicate<Integer> isEvenNumberPredicate = number -> number % 2 == 0;
 		List<Integer> evenNumbers = numbers.stream()
-			   .filter(isEvenNumberPredicate)
+			   .filter(number -> isEvenNUmber(number))
 			   .collect(toList());
 		// void accept(T t);
 		evenNumbers.forEach(System.out::println);
 		
 		
 		
+	}
+
+	private static boolean isEvenNUmber(Integer number) {
+		return number % 2 == 0;
 	}
 
 }
