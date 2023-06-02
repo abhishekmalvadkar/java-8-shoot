80d6f3755656b9911bd636aa971ab2f17cd7ae85
Abhishek Malvadkar <abhishek@dev.com>
2023-06-02 10:25:45 +0530

    Problem-2 : Refactoring

--- java-8-shoot/src/main/java/com/java8/problem/Problem_2.java
+++ java-8-shoot/src/main/java/com/java8/problem/Problem_2.java
@@ -2,6 +2,13 @@ package com.java8.problem;
 
 import java.util.List;
 
+interface StringPredicate {
+	
+	public static boolean isStartWithOne(String numberStr) {
+		return numberStr.startsWith("1");
+	}
+}
+
 public class Problem_2 {
 
 	public static void main(String[] args) {
@@ -14,15 +21,12 @@ public class Problem_2 {
 		List<Integer> numbers = List.of(11, 12, 21, 33, 55 , 131);
 		List<String> startWithOneNumbers = numbers.stream()
 								.map(String::valueOf)
-								.filter(Problem_2::isStartWithOne)
+								.filter(StringPredicate::isStartWithOne)
 								.toList();
 
 		startWithOneNumbers.forEach(System.out::println);
 
 	}
 
-	private static boolean isStartWithOne(String numberStr) {
-		return numberStr.startsWith("1");
-	}
 
 }
