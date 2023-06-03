f5eefce48a496b33dee0293ffa9fd5ec130561f5
Abhishek Malvadkar <abhishek@dev.com>
2023-06-03 13:19:21 +0530

    Problem-3 : Refactoring

--- java-8-shoot/src/main/java/com/java8/problem/Problem_3.java
+++ java-8-shoot/src/main/java/com/java8/problem/Problem_3.java
@@ -1,8 +1,10 @@
 package com.java8.problem;
 
-import java.util.HashMap;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
+
 import java.util.List;
-import java.util.Map;
 
 public class Problem_3 {
 
@@ -14,21 +16,13 @@ public class Problem_3 {
 		 */
 
 		List<Integer> numbers = List.of(7, 1, 5, 2, 1, 5, 6, 6, 7);
-		Map<Integer, Integer> numberToCountMap = new HashMap<>();
-
-		for (Integer number : numbers) {
-			if (numberToCountMap.containsKey(number)) {
-				numberToCountMap.put(number, numberToCountMap.get(number) + 1);
-			} else {
-				numberToCountMap.put(number, 1);
-			}
-		}
-
-		List<Integer> duplicateNumbers = numberToCountMap.keySet()
+		List<Integer> duplicateNumbers = numbers.stream()
+				.collect(groupingBy(number -> number, counting()))
+				.entrySet()
 				.stream()
-				.filter(key -> numberToCountMap.get(key) > 1)
-				.toList();
-
+				.filter(entry -> entry.getValue() > 1)
+				.map(entry -> entry.getKey())
+				.collect(toList());
 
 		System.out.println(duplicateNumbers);
 
