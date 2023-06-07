ff0a20eeab800f816faf132d33979ee5072d352b
Abhishek Malvadkar <abhishek@dev.com>
2023-06-07 09:50:50 +0530

    Problem-6 : Given a list of integers, find the maximum value element present in it using Stream functions?

---
+++ java-8-shoot/src/main/java/com/java8/problem/Problem_6.java
@@ -0,0 +1,32 @@
+package com.java8.problem;
+
+import java.util.List;
+
+class MaxNumberNotFoundException extends RuntimeException{
+	
+	public MaxNumberNotFoundException(String message) {
+		super(message);
+	}
+	
+}
+
+public class Problem_6 {
+
+	public static void main(String[] args) {
+
+		/**
+		 *  Given a list of integers, find the maximum value element present in it using Stream functions?
+		 */
+		
+		List<Integer> numbers = List.of(1,4 , 6 , 10 , 6);
+
+		Integer maxNumber = numbers.stream()
+			   .max((o1,o2) -> o1.compareTo(o2)) // int compare(T o1, T o2);
+			   .orElseThrow(() -> new MaxNumberNotFoundException("Maximum number not found")); // T get();
+		
+		System.out.println(maxNumber);
+
+	}
+
+
+}
